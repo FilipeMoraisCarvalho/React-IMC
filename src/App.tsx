a707479524b7fff67d12229cd480c8cffcb273a7
Filipe Morais <moraisfelipe10@yahoo.com>
2022-06-14 13:39:13 -0300

    Criando rightSide

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from 'react';
+import styles from './App.module.css';
+import powerImage from './assets/powered.png';
+import {levels, calculateImc} from './helpers/imc';
+import { GridItem } from './components/GridItem';
+
+
+
+const App = () => {
+
+  const [heighField, setHeighField] = useState(0);
+  const [weighField, setWeighField] = useState(0);
+
+  const handleCalculateButton = () => {
+    if (heighField && weighField){
+
+
+    }
+
+    else alert('Digite todos os campos');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={powerImage} alt="" width={150} />
+        </div>
       </header>
+
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC.</h1>
+          <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+          
+          <input 
+            placeholder='Digite a sua altura. Ex: 1.5 (em métros)' 
+            type="number" 
+            value={heighField > 0 ? heighField : ''} 
+            onChange={e => setHeighField(parseFloat(e.target.value))}
+          />
+
+          <input 
+            placeholder='Digite o seu peso. Ex: 75.3 (em kg)' 
+            type="number" 
+            value={weighField > 0 ? weighField : ''} 
+            onChange={e => setWeighField(parseFloat(e.target.value))} 
+          />
+
+          <button onClick={handleCalculateButton} >Calcular</button>
+         
+          
+          </div>
+        <div className={styles.rightSide}>
+          <div className={styles.grid}>
+            {levels.map((item, key) => (
+              <GridItem key={key} item={item}/>
+            ))}
+          </div>
+        </div>
+      </div>
+        
     </div>
   );
 }
